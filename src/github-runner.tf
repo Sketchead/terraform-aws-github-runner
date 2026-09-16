2d48abf7205de63c447de35183632df6ebe9a5d9
Atxel Rico <atxel.rico@gmail.com>
2026-09-15 21:03:03 -0700

    Add spot and github runner tf

---
+++ src/github-runner.tf
@@ -0,0 +1,21 @@
+module "github-runner" {
+  source  = "github-aws-runners/github-runner/aws"
+  version = "1"
+
+  aws_region = "eu-west-1"
+  vpc_id     = "vpc-123"
+  subnet_ids = ["subnet-123", "subnet-456"]
+
+  prefix = "gh-ci"
+
+  github_app = {
+    key_base64     = "base64string"
+    id             = "1"
+    webhook_secret = "webhook_secret"
+  }
+
+  webhook_lambda_zip                = "/mnt/d/terraform/aws-runners/download-lambda/webhook.zip"
+  runner_binaries_syncer_lambda_zip = "/mnt/d/terraform/aws-runners/download-lambdarunner-binaries-syncer.zip"
+  runners_lambda_zip                = "/mnt/d/terraform/aws-runners/download-lambdarunners.zip"
+  enable_organization_runners = true
+}
